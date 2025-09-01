b6099c04fe5998d6e753e84588a66eb9a0ff1929
Toshihide Imaruoka <imaru@neptune.kanazawa-it.ac.jp>
2025-09-01 18:40:23 +0900

    0901_2

--- kanazawa2025.R
+++ kanazawa2025.R
@@ -23,10 +23,6 @@ print(paste0('有効回答率:',as.character(100*(all-exl)/all),'%'))
 
 # データの全体像
 
-hist(dat$Q8, ylab='人数', xlab='身長', main='身長の分布')
-hist(dat$Q15, ylab='人数', xlab='平均朝食回数', main='平均回数の分布')
-print(paste0('朝食を食べた人: ',sum(dat$Q14=='食べた'),'/',as.character(all-exl)))
-
 dat<-tdat[(tdat$Q2=='同意する' & tdat$Q9=='辞退しない'),]
 dat$Q11f<-apply(dat,1,function(x){
   temp<-strsplit(x[23],',')
@@ -34,7 +30,19 @@ dat$Q11f<-apply(dat,1,function(x){
   return(temp[[1]][1])
 })
 
-# 性差と朝食
+
+
+hist(dat$Q8, ylab='人数', xlab='身長', main='身長の分布')
+print(paste0(mean(dat$Q8, na.rm = T), '/ ', sd(dat$Q8, na.rm = T)))
+
+hist(dat$Q15, ylab='人数', xlab='平均朝食回数', main='平均回数の分布')
+print(paste0(mean(dat$Q15, na.rm = T), '/ ', sd(dat$Q15, na.rm = T)))
+
+print(paste0('朝食を食べた人: ',sum(dat$Q14=='食べた'),'/',as.character(all-exl)))
+
+
+
+# 性差と朝食：質的データの度数をカテゴリ間で比較
 Q5table<-dat %>% dplyr::group_by(Q5) %>% dplyr::select(Q14) %>% table
 Q5dat<-Q5table %>% as.data.frame()
 g5<-ggplot(data=Q5dat, aes(x=Q5, y=Freq, fill=Q14))+geom_bar(stat="identity",position="dodge")
@@ -43,7 +51,7 @@ g5<-g5+xlab(attr(dat$Q5,'label'))
 print(Q5table)
 plot(g5)
 
-# 性差と平均
+# 性差と平均：量的データの代表値をカテゴリ間で比較
 Q5msummary<- dat %>% group_by(Q5) %>% summarise(count=n(), mean=mean(Q15,na.rm=T), sd=sd(Q15, na.rm=T), median=median(Q15, na.rm=T))
 g5m<-ggplot(data=dat, aes(x=Q5, y=Q15))+geom_boxplot()
 g5m<-g5m+ylab('朝食平均回数')+theme(text=element_text(size=18))
@@ -51,7 +59,7 @@ g5m<-g5m+xlab(attr(dat$Q5,'label'))
 print(Q5msummary)
 plot(g5m)
 
-# 就寝時間と朝食
+# 就寝時間と朝食：度数をカテゴリ間で比較
 Q12table<-dat %>% dplyr::group_by(Q12) %>% dplyr::select(Q14) %>% table
 Q12dat<-Q12table %>% as.data.frame()
 g12<-ggplot(data=Q12dat, aes(x=Q12, y=Freq, fill=Q14))+geom_bar(stat="identity",position="dodge")
@@ -60,14 +68,14 @@ g12<-g12+xlab(attr(dat$Q12,'label'))
 print(Q12table)
 plot(g12)
 
-# 課外活動
+# 課外活動：度数をカテゴリ間で比較
 Q11table<-dat %>% dplyr::group_by(Q11f) %>% dplyr::select(Q14) %>% table
 Q11dat<- Q11table %>% as.data.frame()
 g11<-ggplot(data=Q11dat, aes(x=Q11f, y=Freq, fill=Q14))+geom_bar(stat="identity",position="dodge")
 g11<-g11+xlab(attr(dat$Q11,'label'))+ylab('人数')+theme(text=element_text(size=18))
 plot(g11)
 
-# 身長と平均
+# 身長と平均：量的データ同士の関係を表示
 #Q8table<-dat %>% dplyr::group_by(Q8) %>% dplyr::select(Q15) %>% table
 #Q8dat<- Q8table %>% as.data.frame()
 g8<-ggplot(data=dat, aes(x=Q8, y=Q15))+geom_point(size=5)
@@ -75,7 +83,7 @@ xl<-attr(dat$Q8,'label')
 g8<-g8+xlab(substr(xl,1,2))+ylab('平均回数')+theme(text=element_text(size=18))
 plot(g8)
 
-# 朝食と平均
+# 朝食と平均：見方を変える（縦軸として比較していたものを横軸に）
 Q14msummary<- dat %>% group_by(Q14) %>% summarise(count=n(), mean=mean(Q15,na.rm=T), sd=sd(Q15, na.rm=T), median=median(Q15, na.rm=T))
 g14m<-ggplot(data=dat, aes(x=Q14, y=Q15))+geom_boxplot()
 g14m<-g14m+ylab('朝食平均回数')+theme(text=element_text(size=18))
@@ -83,4 +91,15 @@ g14m<-g14m+xlab('今日の朝食')
 print(Q14msummary)
 plot(g14m)
 
+# 平均2000と500, SDが5000のダミーデータ
 
+datA<-rnorm(1000, mean=2000, sd=5000)
+datB<-rnorm(1000, mean=500, sd=5000)
+
+barplot(c(mean(datA), mean(datB)), ylim=c(0,2000))
+
+wdat<-data.frame(cbind(datA,datB))
+colnames(wdat)<-c('A','B')
+ldat<-pivot_longer(wdat, cols = c('A','B'))
+demoG<-ggplot(data = ldat, aes(x=name, y=value))+geom_boxplot()
+plot(demoG)
