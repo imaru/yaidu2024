dc42a129f6ce1aa0b09bc0091d5a7581ad9160a3
Imaruoka, Toshihide <imaru@neptune.kanazawa-it.ac.jp>
2025-09-01 17:36:42 +0900

    0901

--- kanazawa2025.R
+++ kanazawa2025.R
@@ -9,18 +9,47 @@ rm(list=ls())
 fn<-file.choose()
 tdat<-qualtRics::read_survey(fn,col_types = readr::cols(Q11=readr::col_character()))
 
+# データ数確認
 all<-nrow(tdat)
 cons<-sum(tdat$Q2=='同意する')
 ncons<-sum(tdat$Q2=='同意しない')
 jitai<-sum(tdat$Q2=='同意する' & tdat$Q9=='辞退する')
 exl<-sum(tdat$Q2=='同意しない' | tdat$Q9=='辞退する')
-dat<-tdat[(tdat$Q2=='同意する' & tdat$Q9=='辞退しない'),]
 
+print(paste0('総数:',as.character(all)))
+print(paste0('同意:',as.character(cons)))
+print(paste0('有効:',as.character(all-exl)))
+print(paste0('有効回答率:',as.character(100*(all-exl)/all),'%'))
+
+# データの全体像
+
+hist(dat$Q8, ylab='人数', xlab='身長', main='身長の分布')
+hist(dat$Q15, ylab='人数', xlab='平均朝食回数', main='平均回数の分布')
+print(paste0('朝食を食べた人: ',sum(dat$Q14=='食べた'),'/',as.character(all-exl)))
+
+dat<-tdat[(tdat$Q2=='同意する' & tdat$Q9=='辞退しない'),]
 dat$Q11f<-apply(dat,1,function(x){
   temp<-strsplit(x[23],',')
-  print(temp)
+  # print(temp)
   return(temp[[1]][1])
 })
+
+# 性差と朝食
+Q5table<-dat %>% dplyr::group_by(Q5) %>% dplyr::select(Q14) %>% table
+Q5dat<-Q5table %>% as.data.frame()
+g5<-ggplot(data=Q5dat, aes(x=Q5, y=Freq, fill=Q14))+geom_bar(stat="identity",position="dodge")
+g5<-g5+ylab('人数')+theme(text=element_text(size=18))
+g5<-g5+xlab(attr(dat$Q5,'label'))
+print(Q5table)
+plot(g5)
+
+# 性差と平均
+Q5msummary<- dat %>% group_by(Q5) %>% summarise(count=n(), mean=mean(Q15,na.rm=T), sd=sd(Q15, na.rm=T), median=median(Q15, na.rm=T))
+g5m<-ggplot(data=dat, aes(x=Q5, y=Q15))+geom_boxplot()
+g5m<-g5m+ylab('朝食平均回数')+theme(text=element_text(size=18))
+g5m<-g5m+xlab(attr(dat$Q5,'label'))
+print(Q5msummary)
+plot(g5m)
 
 # 就寝時間と朝食
 Q12table<-dat %>% dplyr::group_by(Q12) %>% dplyr::select(Q14) %>% table
@@ -31,6 +60,13 @@ g12<-g12+xlab(attr(dat$Q12,'label'))
 print(Q12table)
 plot(g12)
 
+# 課外活動
+Q11table<-dat %>% dplyr::group_by(Q11f) %>% dplyr::select(Q14) %>% table
+Q11dat<- Q11table %>% as.data.frame()
+g11<-ggplot(data=Q11dat, aes(x=Q11f, y=Freq, fill=Q14))+geom_bar(stat="identity",position="dodge")
+g11<-g11+xlab(attr(dat$Q11,'label'))+ylab('人数')+theme(text=element_text(size=18))
+plot(g11)
+
 # 身長と平均
 #Q8table<-dat %>% dplyr::group_by(Q8) %>% dplyr::select(Q15) %>% table
 #Q8dat<- Q8table %>% as.data.frame()
@@ -39,10 +75,12 @@ xl<-attr(dat$Q8,'label')
 g8<-g8+xlab(substr(xl,1,2))+ylab('平均回数')+theme(text=element_text(size=18))
 plot(g8)
 
-# 課外活動
-Q11table<-dat %>% dplyr::group_by(Q11f) %>% dplyr::select(Q14) %>% table
-Q11dat<- Q11table %>% as.data.frame()
-g11<-ggplot(data=Q11dat, aes(x=Q11f, y=Freq, fill=Q14))+geom_bar(stat="identity",position="dodge")
-g11<-g11+xlab(attr(dat$Q11,'label'))+ylab('人数')+theme(text=element_text(size=18))
-plot(g11)
+# 朝食と平均
+Q14msummary<- dat %>% group_by(Q14) %>% summarise(count=n(), mean=mean(Q15,na.rm=T), sd=sd(Q15, na.rm=T), median=median(Q15, na.rm=T))
+g14m<-ggplot(data=dat, aes(x=Q14, y=Q15))+geom_boxplot()
+g14m<-g14m+ylab('朝食平均回数')+theme(text=element_text(size=18))
+g14m<-g14m+xlab('今日の朝食')
+print(Q14msummary)
+plot(g14m)
+
 
